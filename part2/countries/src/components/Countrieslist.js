5a4548bcc8719846f9e8e2b9cae80fb7f771b891
Bernhard Poppe <bernhard.poppe@gmail.com>
2021-07-05 18:47:30 +0200

    e 2.14 done

--- part2/countries/src/components/Countrieslist.js
+++ part2/countries/src/components/Countrieslist.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Countrieslist = ({countries, handleShowSingleCountry}) => {
   return(
     <ul>
--- part2/countries/src/components/SingleCountry.js
+++ part2/countries/src/components/SingleCountry.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const SingleCountry = ({country}) => {
+  /*http://api.weatherstack.com/current?access_key=878a22fa69033eb37362c003381fbe25&query=Vienna*/
+  const api_key = process.env.REACT_APP_API_KEY
+
+  const [ weatherData, setWeatherData ] = useState([])
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=878a22fa69033eb37362c003381fbe25&query=${country.capital}`)
+      .then(response => {
+        console.log('Weather loaded.')
+        setWeatherData(response.data)
+      })
+  }, [])
+
+  console.log(weatherData.current.temperature);
+
   return(
     <div>
       <h1>
@@ -17,6 +35,12 @@ const SingleCountry = ({country}) => {
           )}
       </ul>
       <img src={country.flag} style={{width: '200px'}} />
+      <h1>
+        Weather in {country.capital}
+      </h1>
+       <p><b>temperature: </b>{weatherData.current.temperature} Celsius</p><br/>
+       <img src={weatherData.current.weather_icons[0]} style={{width: '100px'}} /><br/>
+       <p><b>wind: {weatherData.current.temperature}</b></p>
     </div>
   )
 }
