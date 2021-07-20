b9e5fc5c68ee9ac227ca2cd2c38d06f8fc3a04dd
Bernhard Poppe <bernhard.poppe@gmail.com>
2021-07-20 08:59:23 +0200

    e 3.4 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,7 +45,10 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  console.log(request.params.id)
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+  console.log(persons)
 })
 
 app.get('/info', (request, response) => {
